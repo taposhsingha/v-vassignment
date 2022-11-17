e9c50df2a90b24e7d9c8aa004202d58ef3a31789
taposhsingha <57071916+taposhsingha@users.noreply.github.com>
2022-11-17 20:33:54 +0600

    done

---
+++ src/main/java/Admin.java
@@ -0,0 +1,14 @@
+public class Admin {
+    private int Id;
+    private String Char;
+
+    private void ViewProducts(){
+
+    }
+    private void AddProducts(int Id){}
+    private void DeleteProducts(int Id){}
+    private void ModifyProducts(int Id){}
+    private void MakeShipment(){}
+    private void ConfirmDelivery(){}
+
+}
